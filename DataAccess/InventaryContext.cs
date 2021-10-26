ac326f486e5b3321c9be9b7cf3d9039265ddfd9b
rolando <rolan.salas02@gmail.com>
2021-10-26 22:49:40 +0200

    About writing the method to load data

--- DataAccess/InventaryContext.cs
+++ DataAccess/InventaryContext.cs
@@ -15,7 +15,7 @@ namespace DataAccess
         public DbSet<WarehouseEntity> WareHouses { get; set; }
         public DbSet<StoregeEntity> Storages { get; set; }
 
-        //Sobre escribir el metodo 
+        //Sobre escribir el metodo de configuracion hacia la BD
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             if (!options.IsConfigured)
@@ -23,5 +23,26 @@ namespace DataAccess
                 options.UseSqlServer("Server = DESKTOP-JV6441Q; Database = InventoryDb; User Id =sa; Password =4750619Aa ");            
             }
         }
+        //Sobre escribir metodo para cargar datos 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CategoryEntity>().HasData(
+                new CategoryEntity { CategotyId = "ASH", CategoryName= "Aseo Hogar"},
+                new CategoryEntity { CategotyId = "ASP", CategoryName= "Aseo Personal"},
+                new CategoryEntity { CategotyId = "HGR", CategoryName= "Hogar"},
+                new CategoryEntity { CategotyId = "PRF", CategoryName= "Perfumeria"},
+                new CategoryEntity { CategotyId = "SLD", CategoryName= "Salud"},
+                new CategoryEntity { CategotyId = "VDJ", CategoryName= "Video Juegos"}
+                );
+
+            modelBuilder.Entity<WarehouseEntity>().HasData(
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Central",WarehouseAddres = "Calle Antezana" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Norte",WarehouseAddres = "Calle Atahuallapa" },
+                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Sud",WarehouseAddres = "Calle Villa Pagador" }
+    );
+        }
+      
     }
 }
